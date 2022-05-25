04e83414a237430e3a5cbf6e9ae5e1da6f4170cd
xtaodada <xtao@xtaolink.cn>
2022-05-25 11:58:04 +0800

    🐛 Not use of a deprecated module

--- pagermaid/config.py
+++ pagermaid/config.py
@@ -3,7 +3,22 @@ from json import load as load_json
 import sys
 from yaml import load, FullLoader, safe_load
 from shutil import copyfile
-from distutils.util import strtobool
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
 
 
 try:
