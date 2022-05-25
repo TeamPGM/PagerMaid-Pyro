f048f3e43ab327849b6ab390d19d7315ba093c5b
xtaodada <xtao@xtaolink.cn>
2022-05-25 21:50:01 +0800

    support debug log

--- pagermaid/__init__.py
+++ pagermaid/__init__.py
@@ -1,5 +1,5 @@
 from coloredlogs import ColoredFormatter
-from logging import getLogger, StreamHandler, CRITICAL, INFO, basicConfig
+from logging import getLogger, StreamHandler, CRITICAL, INFO, basicConfig, DEBUG
 from datetime import datetime
 from os import getcwd
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -27,8 +27,8 @@ logging_handler.setFormatter(ColoredFormatter(logging_format))
 root_logger = getLogger()
 root_logger.setLevel(CRITICAL)
 root_logger.addHandler(logging_handler)
-basicConfig(level=INFO)
-logs.setLevel(INFO)
+basicConfig(level=DEBUG if Config.DEBUG else INFO)
+logs.setLevel(DEBUG if Config.DEBUG else INFO)
 
 # easy check
 if not Config.API_ID:
