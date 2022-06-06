56cddb15c8749d1d80f7ecca4a802709a92ac46b
omg-xtao <100690902+omg-xtao@users.noreply.github.com>
2022-06-06 23:06:17 +0800

    Update user-agent

--- pagermaid/utils.py
+++ pagermaid/utils.py
@@ -193,7 +193,6 @@ def check_manage_subs(message: Message) -> bool:
 """ Init httpx client """
 # 使用自定义 UA
 headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/102.0.5005.72 Safari/537.36 "
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.72 Safari/537.36"
 }
 client = httpx.AsyncClient(timeout=10.0, headers=headers)
